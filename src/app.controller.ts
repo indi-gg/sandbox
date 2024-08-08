153fa560f1cba544cf293f9d3c5c03474886095d
Abhishek Deshkar <abhishek.deshkar@indi.gg>
2024-08-08 16:18:18 +0530

    mf

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,16 +2,19 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
-export class AppController {
+export class AppController
+{
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHealthCheck(): string
+  {
+    return this.appService.getServiceHealthCheck();
   }
 
   @Get('env/:key')
-  getEnvValue(@Param('key') key: string): string {
+  getEnvValue(@Param('key') key: string): string
+  {
     return process.env[key] || 'Key not found';
   }
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService
+{
+  getServiceHealthCheck(): string
+  {
+    return 'API is up and running : Version 1.0.0';
   }
 }
